127d61cd3d643af3fa2b1d32f466af342d95ce06
wilyan <92753649+wilyanmatos@users.noreply.github.com>
2023-08-29 18:58:06 -0300

    Novas práticas do curso de javascript

--- Aulas/aula_08/aula_08.js
+++ Aulas/aula_08/aula_08.js
@@ -1,6 +1,23 @@
 // Operadores Bitwise
-let n1 = 10
+let n1 = 100
 let n2 = 11
 
-let res = n1 & n2
-console.log(res) 
+//and
+let res1 = n1 & n2
+console.log(res1) 
+
+//or
+let res2 = n1 | n2
+console.log(res2) 
+
+//sor
+let res3 = n1 ^ n2
+console.log(res3)
+
+// << Descola zero para a esquerda
+let res4 = n1 << 1
+console.log(res4)
+
+// >> Desloca o zero para a direita
+let res5 = n1 >> 2
+console.log(res5)
---
+++ Aulas/aula_09/aula_09.js
@@ -0,0 +1,12 @@
+// Pré e Pós incremento e o Operador de inversão
+//Pos incremento
+let num = 1
+console.log(num ++)
+console.log(num)
+
+//Pre incremento
+console.log(++num)
+
+//Operador de inversão 
+let x = -num
+console.log(x)
