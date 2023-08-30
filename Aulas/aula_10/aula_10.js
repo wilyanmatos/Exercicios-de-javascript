307255d1a2f547a8b65a9c3df14756aac7037e55
wilyan <92753649+wilyanmatos@users.noreply.github.com>
2023-08-30 11:50:52 -0300

    Novas praticas do curso de javascript

---
+++ Aulas/aula_10/aula_10.js
@@ -0,0 +1,13 @@
+// Operador ternário
+// 0 = False e 1 = True
+
+//Na prática 01
+let num = 2
+res = (num % 2 ? "ímpar" : "Par")
+console.log(res)
+
+// Na prática 02
+let num1 = 5
+let num2 = 15
+res = (num1 > num2 ? "Verdadeiro": "Falso")
+console.log(num1 + " é maior que " + num2 + " ? " + res)
---
+++ Aulas/aula_11/aula_11.js
@@ -0,0 +1,10 @@
+// Typeof
+let v1 = 10
+let v2 = "10"
+let v3 = v1 === v2
+let v4 = {nome : "Wilson"}
+
+console.log("Valor: " + v1 + " - Tipo: " + typeof(v1))
+console.log("Valor: " + v2 + " - Tipo: " + typeof(v2))
+console.log("Valor: " + v3 + " - Tipo: " + typeof(v3))
+console.log("Valor: " + v4 + " - Tipo: " + typeof(v4))
