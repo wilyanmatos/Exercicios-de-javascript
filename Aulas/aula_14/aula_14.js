7ddfe95402f03e10ac7e24dff5793933922e2ba8
wilyan <92753649+wilyanmatos@users.noreply.github.com>
2023-08-31 11:55:31 -0300

    Novo pratica da aula de Javascript

---
+++ Aulas/aula_14/aula_14.js
@@ -0,0 +1,26 @@
+// Switch case
+let colocacao = 4
+
+switch(colocacao){
+    case 1:
+        console.log("Primeiro lugar!")
+        break
+
+    case 2:
+        console.log("Segundo lugar!")
+        break
+    
+    case 3:
+        console.log("Terceiro colocado!")
+        break
+    
+    case 4: case 5: case 6:
+        console.log("Prêmio de participação")
+        break
+
+    default:
+        console.log("O importante é participar")
+        break
+
+}  
+
