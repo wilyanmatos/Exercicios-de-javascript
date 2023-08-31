4215c42dee5b119afe31976f6c6b402fb2e980cc
wilyan <92753649+wilyanmatos@users.noreply.github.com>
2023-08-31 16:12:19 -0300

    Nova pratica das aulas de Javascript

---
+++ Aulas/aula_15/aula_15.js
@@ -0,0 +1,15 @@
+//Loop For
+let var_cont = 0
+for(var_cont; var_cont < 10; var_cont++){
+    console.log(var_cont)
+}
+
+// Na pratica 02
+for(let var_cont = 0; var_cont < 50; var_cont++){
+    if((var_cont % 2) == 0){
+        console.log(var_cont + ' é um número par')
+
+    }else{
+        console.log(var_cont + " é um número Ímpar")
+    }
+}   
